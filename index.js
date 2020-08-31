8001c5c6d0f1713d0cb8e4b69e50929424bb651e
falahsutawindaya-coder <mrfalahsecretemail@email.com>
2020-08-31 18:19:45 +0700

    add js

---
+++ index.js
@@ -0,0 +1,18 @@
+function fadeIn(element, duration = 1600) {
+  element.style.display = "";
+  element.style.opacity = 0;
+  var last = +new Date();
+  var tick = function () {
+    element.style.opacity =
+      +element.style.opacity + (new Date() - last) / duration;
+    last = +new Date();
+    if (+element.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
+    }
+  };
+  tick();
+}
+
+const container = document.querySelector(".container");
+fadeIn(container);
